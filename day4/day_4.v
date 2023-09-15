9425f2e272e6c79617bba718a9266ea6e7aba664
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2023-09-15 14:04:55 +0530

    Update day_4.v

--- day4/day_4.v
+++ day4/day_4.v
@@ -2,11 +2,12 @@
 
 module ring_counter(
     output reg[3:0] out,
-    input clk, reset
+    input clk, reset, preset
     );
     
     always@(posedge clk) begin
-        if(reset) out <= 4'b0001;
+        if(reset) out <= 4'b0000;
+        else if(preset) out <= 4'b0001;
         else out <= (out[3] & 1'b1)? 4'b0001: {out[2:0], 1'b0};
     end
     
