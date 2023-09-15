bfef0d962d1f5a9d6991585226c2709e278e77c8
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2023-09-15 14:04:41 +0530

    Update day_4_tb.v

--- day4/day_4_tb.v
+++ day4/day_4_tb.v
@@ -2,16 +2,18 @@
 
 module tb_ring_counter();
     wire[3:0] out;
-    reg clk, reset;
+    reg clk, reset, preset;
     
-    ring_counter DUT(out, clk, reset);
+    ring_counter DUT(out, clk, reset, preset);
     
     always #10 clk = ~clk;
     
     initial begin
         clk = 0;
+        preset = 1;
         reset = 1;
         
-        #20 reset = 0;    
+        #20 reset = 0;
+        #20 preset = 0;    
     end
 endmodule
