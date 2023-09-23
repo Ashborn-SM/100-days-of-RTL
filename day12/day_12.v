f0078dd0ef0d994f5287571247b1208f2b18a207
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2023-09-23 22:55:47 +0530

    Update day_12.v

--- day12/day_12.v
+++ day12/day_12.v
@@ -1,1 +1,17 @@
+`timescale 1ns / 1ps
 
+module circular_shifter_8(
+    output[7:0] out,
+    input[7:0] in,
+    input[2:0] shift
+    );
+    integer i;
+    reg[7:0] temp;
+    always@(*) begin
+        temp = in;
+        for(i=0; i<shift; i=i+1) begin
+            temp = {temp[0], temp[7:1]};
+        end
+    end
+    assign out = temp;
+endmodule
