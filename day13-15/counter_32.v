2bb8a94e76d86e4b4b60561452b1f5324c65254a
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2023-09-25 23:16:53 +0530

    Update counter_32.v

--- day13-15/counter_32.v
+++ day13-15/counter_32.v
@@ -7,6 +7,7 @@ module counter_32(
     reg[31:0] counter;
     always@(posedge clk) begin
         if(reset) counter = {32{1'b0}};
-        else counter = counter + 1;
+        else counter <= counter + 1;
     end
+    assign out = counter;
 endmodule
