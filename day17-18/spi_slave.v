4a920a8351a51d5f32267b483a35352eb8f3b097
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2023-10-01 22:56:08 +0530

    Update spi_slave.v

--- day17-18/spi_slave.v
+++ day17-18/spi_slave.v
@@ -34,9 +34,9 @@ module slave(
         output reg      o_MISO, 
         
         // TX (MISO) Signals
-        output reg      o_TX_READY, // Ready for next byte Transmission
+        output reg      o_TX_DV,    // Finished transmission
         input[7:0]      i_TX_DATA,  // Data to be transmiited to master
-        input[7:0]      i_TX_DV,    // To register i_TX_DATA (1 cc pulse)
+        input           i_TX_DV,    // To register i_TX_DATA (1 cc pulse)
         
         // RX (MOSI) Singals
         output reg      o_RX_DV,    // Data received pulse
@@ -46,40 +46,65 @@ module slave(
     reg[2:0]        rx_counter;
     reg[2:0]        tx_counter;
     reg[7:0]        tx_data;
+    reg[7:0]        rx_data;
+    reg             r_MISO;
+    reg             r_TX_DV;
+    reg             r_RX_DV;
+    reg             begin_TX;
+    reg             delay_0;
+    reg[1:0]        begin_counter;
+    reg             r_MISO;
+    reg             r2_RX_DV;
+    
+    //  MISO
+    always@(posedge S_CLK, negedge delay_0) begin
+        if(begin_TX) begin
+            tx_counter <= 3'b110;
+            o_MISO <= tx_data[3'b111];
+            r_TX_DV <= 1'b0;
+        end
+        else begin
+            r_TX_DV <= 1'b0;
+            o_MISO = tx_data[tx_counter];
+            tx_counter <= tx_counter - 1;
+            if(tx_counter == 3'b000) r_TX_DV <= 1'b1;
+        end
+    end
+    
+    always@(negedge P_CLK) begin
+        if(~i_SS & delay_0) begin_TX <= 1'b1;
+        else begin_TX <= 1'b0;
+    end
+
+    always@(posedge P_CLK, posedge reset) begin
+        if(reset) begin
+            tx_data <= 8'h0;
+            o_TX_DV <= 1'b0;
+            o_RX_DATA <= 8'h0;
+        end
+        else begin
+            delay_0 <= i_SS;
+            if(i_TX_DV) tx_data <= i_TX_DATA;    
+            o_TX_DV <= (i_SS | begin_TX)? 1'b0: r_TX_DV;
+            r2_RX_DV <= r_RX_DV;
+            o_RX_DV <= i_SS? 1'b0: r2_RX_DV;
+            o_RX_DATA <= i_SS? o_RX_DATA: rx_data;
+        end
+    end
     
     // MOSI
-    always@(posedge S_CLK, posedge i_SS) begin
+    always@(negedge S_CLK, posedge i_SS) begin
         if(i_SS) begin
-            o_RX_DV <= 1'b0;
-            o_RX_DATA <= 8'h0;
-            rx_counter <= 3'h0;
+            rx_counter <= 3'b111;
+            rx_data <= 8'h0;
+            r_RX_DV <= 1'b0;
         end
         else begin
-            o_RX_DV <= 1'b0;
-            o_RX_DATA <= {o_RX_DATA[6:0], i_MOSI};
-            rx_counter <= rx_counter + 1;
-            if(rx_counter == 3'b111) o_RX_DV <= 1'b1;
-        end 
-    end
-    
-    // MISO
-    always@(posedge S_CLK, posedge i_SS) begin
-        if(i_SS) begin
-            o_MISO <= tx_data[3'b111];
-            tx_counter <= 3'b110;
-            o_TX_READY <= 1'b0;
-        end
-        else begin
-            o_MISO <= tx_data[tx_counter];
-            tx_counter <= tx_counter - 1;
-            if(tx_counter == 3'b000) o_TX_READY <= 1'b1;
+            r_RX_DV <= 1'b0;
+            rx_counter <= rx_counter - 1;
+            rx_data <= {rx_data[6:0], i_MOSI};
+            if(rx_counter === 3'b000) r_RX_DV <= 1'b1;
         end
     end
-    
-    always@(posedge P_CLK, negedge reset) begin
-        if(reset)  tx_data <= 8'h0;    
-        else if(i_TX_DV) tx_data <= i_TX_DATA;
-    end
-    
     
 endmodule
